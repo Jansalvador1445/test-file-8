0f5acd3b9c1b3dc8a0ad55a576b3b4dbeb0b16ff
Jan Salvador Sebastian <sebastian@mclinica.com>
2024-08-31 15:43:10 +0800

    Added variables

--- Main.java
+++ Main.java
@@ -1,7 +1,12 @@
 class Main{
     public static void main(String[] args) {
-        
+
+
+        String x = "Hello World";
+        System.out.println(x);
         System.out.println("Hello World");
+
+
         
     }   
 }
